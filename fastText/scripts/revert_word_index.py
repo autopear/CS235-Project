4f3efd566eb39153b1b9d85f0c9a04c9cd369898
Merlin <lonelystars@gmail.com>
2017-10-25 21:20:27 -0700

    Add fastText

---
+++ fastText/scripts/revert_word_index.py
@@ -0,0 +1,60 @@
+# fastText accepts direct words, so revert 5 groups back to word instead of word indices
+
+import os
+
+num_folds = 5
+
+
+def to_system_path(path):
+    if os.name == "nt":
+        return path.replace("/", "\\")
+    else:
+        return path.replace("\\", "/")
+
+
+def to_standard_path(path):
+    return path.replace("\\", "/")
+
+
+dir_path = to_standard_path(os.path.dirname(os.path.realpath(__file__)))
+dir_path = "/".join(dir_path.split("/")[:-1])
+output_dir = "{0}/data".format(dir_path)
+input_dir = "{0}/Samples".format("/".join(dir_path.split("/")[:-1]))
+word_map_path = to_system_path("{0}/word_map.tsv".format(input_dir))
+fold_prefix = to_system_path("{0}/5-fold/fold-".format(input_dir))
+
+word_map = {}
+with open(word_map_path, "r") as inf:
+    for line in inf:
+        line = line[:-1]
+        if len(line) < 1:
+            continue
+        components = line.split("\t")
+        idx = int(components[0])
+        word = components[1]
+        word_map[idx] = word
+inf.close()
+print("Word map loaded")
+
+for i in range(1, num_folds+1):
+    fold_path = "{0}{1}.tsv".format(fold_prefix, i)
+
+    outf = open(to_system_path("{0}/fold-{1}.tsv".format(output_dir, i)), "w")
+
+    with open(fold_path, "r") as inf:
+        for line in inf:
+            line = line[:-1]
+            if len(line) < 1:
+                continue
+            components = line.split("\t")
+            doc_idx = int(components[0])
+            score = int(components[1])
+            word_indices = components[2].split(" ")
+            words = []
+            for idx in word_indices:
+                words.append(word_map[int(idx)])
+            outf.write("{0}\t{1}\t{2}\n".format(doc_idx, score, " ".join(words)))
+    inf.close()
+    outf.close()
+    print("Done fold {0}".format(i))
+print("Done")
